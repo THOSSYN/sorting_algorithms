b2b0eb90573d3a96dc2af21a400310fa128f06b8
THOSSYN <mtafortunefinder@gmail.com>
2023-07-21 11:42:58 -0700

    Update: All files

--- sort.h
+++ sort.h
@@ -33,12 +33,20 @@ void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
+
+/**
+ * enum kind_e - enumerate
+ * @SPADE: is the spade suit
+ * @HEART: is the heart suit
+ * @CLUB: is the club suit
+ * @DIAMOND: is the diamond suit
+ */
 typedef enum kind_e
 {
-    SPADE = 0,
-    HEART,
-    CLUB,
-    DIAMOND
+	SPADE = 0,
+	HEART,
+	CLUB,
+	DIAMOND
 } kind_t;
 
 /**
@@ -50,8 +58,8 @@ typedef enum kind_e
  */
 typedef struct card_s
 {
-    const char *value;
-    const kind_t kind;
+	const char *value;
+	const kind_t kind;
 } card_t;
 
 /**
@@ -63,54 +71,9 @@ typedef struct card_s
  */
 typedef struct deck_node_s
 {
-    const card_t *card;
-    struct deck_node_s *prev;
-    struct deck_node_s *next;
-} deck_node_t;#include "sort.h"
-
-/**
- * swap - exchange one value with another
- * @value1: is the value to exchange
- * @value2: is the value to exchange with
- * Return: Nothing
- */
-
-void swap(int *value1, int *value2)
-{
-        int tmp;
-
-        tmp = *value1;
-        *value1 = *value2;
-        *value2 = tmp;
-}
-
-/**
- * bubble_sort - sorts an array in ascending order
- * @array: is the array containing the data
- * @size: size of the array
- * Return: nothing
- */
-
-void bubble_sort(int *array, size_t size)
-{
-        size_t i, j;
-
-        i = size - 1;
-        while (i > 0)
-        {
-                j = 0;
-                while (j < i)
-                {
-                        if (array[j] > array[j + 1]) 
-                        {
-                                swap(&array[j], &array[j + 1]);
-                                print_array(array, size);
-                        }
-                        j++;
-                }
-                i--;
-        }
-}
-void sort_deck(deck_node_t **deck);
+	const card_t *card;
+	struct deck_node_s *prev;
+	struct deck_node_s *next;
+} deck_node_t;
 
 #endif /* SORT_H */
