5639e3d96f74ea06cee9f4591f75a6545437fc99
THOSSYN <mtafortunefinder@gmail.com>
2023-07-22 02:34:40 -0700

    Updated: sort.h

--- sort.h
+++ sort.h
@@ -25,6 +25,8 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+size_t pivot_function(int *array, size_t low, size_t size);
+void Re_quick_sort(int *array, size_t low, size_t size);
 void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
 void counting_sort(int *array, size_t size);
