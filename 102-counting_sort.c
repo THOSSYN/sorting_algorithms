c5713d0b734d1edfa01cb6bb83ee505762c1ca57
THOSSYN <mtafortunefinder@gmail.com>
2023-07-22 02:49:28 -0700

    corrected: 102-counting_sort.c

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -19,22 +19,16 @@ void counting_sort(int *array, size_t size)
 		if (array[i] > max)
 			max = array[i];
 	}
-
 	element_count = malloc(sizeof(int) * (max + 1));
 	if (element_count == NULL)
-	{
-		/*free(element_count);*/
 		return;
-	}
 	/*fill the malloc'd space for elemnt_count array with 0's */
 	for (j = 1; j <= max; j++)
 		element_count[j] = 0;
-	/*Now start filling the element_count array with the frequency
-	 *using the array[i] of each element to index element_count array
-	 */
+	/*Now start filling the element_count array with the frequency*/
+	/*using the array[i] of each element to index element_count array*/
 	for (i = 0; i < size; i++)
 		++element_count[array[i]];
-
 	for (j = 0; j <= max; j++)
 	{
 		element_count[j] += element_count[j - 1];
@@ -46,14 +40,12 @@ void counting_sort(int *array, size_t size)
 		free(element_count);
 		return;
 	}
-
 	for (i = size - 1; i > 0; i--)
 	{
 		resolved_array[--element_count[array[i]]] = array[i];
 	}
 	for (i = 0; i < size; i++)
 		array[i] = resolved_array[i];
-
 	free(element_count);
 	free(resolved_array);
 }
